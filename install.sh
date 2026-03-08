b26a537b4edde7f7db6d99a3c62d0ea06d241feb
A1-EVO <ingacheva2012@gmail.com>
2026-03-08 21:08:02 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+# Куда ставим меню
+INSTALL_DIR="/usr/local/vps-menu"
+
+# Создаём папку если нет
+mkdir -p "$INSTALL_DIR"
+
+# Качаем последнюю версию из GitHub
+curl -L https://raw.githubusercontent.com/A1-EVO/VPS/main/vps-menu.sh -o "$INSTALL_DIR/vps-menu.sh"
+
+# Делаем исполняемым
+chmod +x "$INSTALL_DIR/vps-menu.sh"
+
+# Создаём команду start
+ln -sf "$INSTALL_DIR/vps-menu.sh" /usr/local/bin/start
+
+echo "Установка завершена. Запускайте командой: start"
