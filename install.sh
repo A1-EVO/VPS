d94150f0a5e099b8fd7f3024428c9d1e73b5334d
A1-EVO <ingacheva2012@gmail.com>
2026-03-09 09:37:45 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,10 +11,16 @@ mkdir -p "$INSTALL_DIR"
 # Качаем последнюю версию из GitHub
 curl -L "https://raw.githubusercontent.com/A1-EVO/VPS/main/vps-menu.sh?$(date +%s)-$RANDOM" \
     -o "$INSTALL_DIR/vps-menu.sh"
-
-
 # Делаем исполняемым
 chmod +x "$INSTALL_DIR/vps-menu.sh"
+
+
+# Качаем панель ретро
+curl -L "https://raw.githubusercontent.com/A1-EVO/VPS/main/sys-panel.sh?$(date +%s)-$RANDOM" \
+    -o "$INSTALL_DIR/sys-panel.sh"
+# Делаем исполняемым
+chmod +x "$INSTALL_DIR/sys-panel.sh"
+
 
 # Создаём команду start
 ln -sf "$INSTALL_DIR/vps-menu.sh" /usr/local/bin/start
